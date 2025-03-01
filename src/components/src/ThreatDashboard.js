65da0861de0f40c09daa0a56350924be7eb2594a
Montana Nicholson <nicholsonmontana@gmail.com>
2025-03-01 05:43:45 -0600

    Header Adjustments
    Adjusting the header so that the background is behind it.

--- src/components/src/ThreatDashboard.js
+++ src/components/src/ThreatDashboard.js
@@ -4,9 +4,9 @@ import './ThreatDashboard.css';
 function ThreatDashboard() {
     return (
         <div className="ThreatDashboard">
-            <h1>Real-Time Threat Intelligence</h1>
-            <p>Live Threat Updates will be displayed here.</p>
             <header className="ThreatDashboard-header">
+                <h1>Real-Time Threat Intelligence</h1>
+                <p>Live Threat Updates will be displayed here.</p>
                 <img src={logo} className="ThreatDashboard-logo" alt="logo" />
                 <p>
                     Edit <code>src/ThreatDashboard.js</code> and save to reload.
