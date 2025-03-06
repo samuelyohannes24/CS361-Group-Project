ecefdacc58574a3c30398015fabac22cf2b4cd3b
toan28102002 <tqndvt@umsystem.edu>
2025-03-05 19:37:41 -0600

    Removed embedded git repository

---
+++ threat-dashboard/src/App.js
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to Threat Intelligence Platform</h1>
+            <div>
+                <button>Login</button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
