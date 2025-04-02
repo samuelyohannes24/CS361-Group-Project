d1d412b73b2126940cc26cb013958605b5a36fbc
samuelyohannes24 <58994918+samuelyohannes24@users.noreply.github.com>
2025-04-01 20:49:24 -0500

    Create incident_response.js

---
+++ api/incident_response.js
@@ -0,0 +1,6 @@
+// Blue Teaming & Incident Response
+function generateIncidentResponsePlan(threat) {
+    return `Incident Response Plan for ${threat}: 1. Identify the attack vector. 2. Isolate affected systems. 3. Apply mitigation. 4. Report incident.`;
+}
+
+module.exports = { generateIncidentResponsePlan };
